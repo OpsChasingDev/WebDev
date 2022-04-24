7a32a8d418011505b1755aae5e7460770ba461a2
drummermanrob20 <robert@stapleton-family.net>
2022-04-24 10:10:38 -0400

    modifying opacity

--- BlurryImage/script.js
+++ BlurryImage/script.js
@@ -1,5 +1,15 @@
 const BackgroundImage = document.querySelector('.BackgroundImage')
 const PercentText = document.querySelector('.PercentText')
 
-// console.log(BackgroundImage)
-//console.log(PercentText)
+let LoadNumber = 0
+let Interval = setInterval(BlurEffect, 20)
+
+function BlurEffect() {
+    if (LoadNumber < 100) {
+        LoadNumber++
+        console.log(LoadNumber)
+
+        PercentText.innerText = `${LoadNumber}%`
+        PercentText.style.opacity = .5
+    }
+}
