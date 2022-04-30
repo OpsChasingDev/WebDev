3ed47bebe4aebe0d04d1120d3b37a9bd883fd7a3
drummermanrob20 <robert@stapleton-family.net>
2022-04-30 10:56:22 -0400

    call document elements

---
+++ VerticleSlider/script.js
@@ -0,0 +1,7 @@
+const SliderContainer = document.querySelector('.SliderContainer')
+const SliderLeft = document.querySelector('.SliderLeft')
+const SliderRight = document.querySelector('.SliderRight')
+const ButtonLeft = document.querySelector('ButtonLeft')
+const ButtonRight = document.querySelector('ButtonRight')
+const SlideNumber = SliderRight.querySelectorAll('div').length
+
