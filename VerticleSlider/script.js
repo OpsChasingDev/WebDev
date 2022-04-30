90abbab8115e2c696bc1fc5928770882aeed47d8
drummermanrob20 <robert@stapleton-family.net>
2022-04-30 11:09:40 -0400

    initial left and right content aligned

--- VerticleSlider/script.js
+++ VerticleSlider/script.js
@@ -1,7 +1,10 @@
 const SliderContainer = document.querySelector('.SliderContainer')
 const SliderLeft = document.querySelector('.SliderLeft')
 const SliderRight = document.querySelector('.SliderRight')
-const ButtonLeft = document.querySelector('ButtonLeft')
-const ButtonRight = document.querySelector('ButtonRight')
+const ButtonLeft = document.querySelector('.ButtonLeft')
+const ButtonRight = document.querySelector('.ButtonRight')
 const SlideNumber = SliderRight.querySelectorAll('div').length
 
+let ActiveSlideIndex = 0
+
+SliderLeft.style.top = `-${(SlideNumber - 1) * 100}vh`
